246ccd69f38a89191836ddf054234e09f715d688
filipovsky <filipovskii.off@gmail.com>
2013-12-18 23:42:04 +0400

    Menu styling.

--- client/app/scripts/controllers/me.js
+++ client/app/scripts/controllers/me.js
@@ -2,9 +2,15 @@
 
 angular.module('clientApp')
   .controller('MeCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    var images = [
+      'bear',
+      'bullfinch',
+      'deer',
+      'dm',
+      'penguin',
+      'snowman' ];
+    $scope.getImage = function () {
+      var n = Math.floor(Math.random() * 100) % (images.length - 1);
+      return images[n];
+    };
   });
