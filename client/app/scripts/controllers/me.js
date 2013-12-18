dc98894ffa8373184117a9987247c70b01fdbe34
filipovsky <filipovskii.off@gmail.com>
2013-12-19 00:56:46 +0400

    My letter.

--- client/app/scripts/controllers/me.js
+++ client/app/scripts/controllers/me.js
@@ -1,16 +1,40 @@
 'use strict';
 
 angular.module('clientApp')
-  .controller('MeCtrl', function ($scope) {
+  .controller('MeCtrl', function ($scope, $location, Fb) {
     var images = [
       'bear',
       'bullfinch',
       'deer',
       'dm',
       'penguin',
-      'snowman' ];
+      'snowman'
+    ];
+
     $scope.getImage = function () {
-      var n = Math.floor(Math.random() * 100) % (images.length - 1);
-      return images[n];
+      var n = Math.floor(Math.random() * 100) % (images.length - 1),
+          image = $scope.image;
+
+      if (image && image.length > 0) { return $scope.image; }
+
+      $scope.image = images[n];
+      return $scope.image;
     };
+
+    Fb.getLoginStatus().pipe(function (res) {
+      if (res.status !== 'connected') {
+        $scope.$apply(function () {
+          $location.path('/where-to-send');
+        });
+        return;
+      }
+
+      return Fb.getUser();
+    }).then(function (user) {
+      console.log('Got user');
+      $scope.$apply(function () {
+        $scope.user = user;
+      });
+    });
+
   });
--- client/app/scripts/services/fb.js
+++ client/app/scripts/services/fb.js
@@ -21,6 +21,22 @@ angular.module('clientApp')
       return d.promise();
     };
 
+
+    service.getUser = function getUser() {
+      var d = $.Deferred();
+
+      function onFbReady() {
+        window.FB.api('/me', function (o) {
+          console.log('Got user', o);
+          d.resolve(o);
+        });
+      };
+
+      service.runWhenReady(onFbReady);
+
+      return d.promise();
+    };
+
     service.runWhenReady = function runWhenReady(fn) {
       if (_.has(window, 'FB')) {
         fn();
