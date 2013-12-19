a5a8e5d8f63177ee168f699d5a3b67ecf669627a
filipovsky <filipovskii.off@gmail.com>
2013-12-19 15:34:43 +0400

    Add/Remove wish.

--- client/app/scripts/controllers/me.js
+++ client/app/scripts/controllers/me.js
@@ -13,6 +13,22 @@ angular.module('clientApp')
       'snowman'
     ];
 
+    $scope.wishlist = [
+      { descr: 'Матрас' },
+      { descr: 'Жвачка' },
+      { descr: 'Хобот' }
+    ];
+
+    $scope.removedClass = function (wish) {
+      if (wish.$markRemoved) { return 'removed'; }
+      return '';
+    };
+
+    $scope.triggerRemoved = function (wish) {
+      console.log('in trigger removed', wish);
+      wish.$markRemoved = !wish.$markRemoved;
+    };
+
     $scope.getImage = function () {
       var n = Math.floor(Math.random() * 100) % (images.length - 1),
           image = $scope.image;
@@ -21,6 +37,26 @@ angular.module('clientApp')
 
       $scope.image = images[n];
       return $scope.image;
+    };
+
+    $scope.addingStarted = function () {
+      return $scope.$adding;
+    }
+
+    $scope.startAdd = function () {
+      $scope.$adding = true;
+    };
+
+    $scope.cancelAdd = function ($ev) {
+      $ev.stopPropagation();
+      $scope.$adding = false;
+      $scope.newWish = '';
+    };
+
+    $scope.finishAdd = function () {
+      $scope.wishlist.push({ descr: $scope.newWish });
+      $scope.$adding = false;
+      $scope.newWish = '';
     };
 
 
