a237d42dc5f74ba403c902ed028072ff7c6a547d
filipovsky <filipovskii.off@gmail.com>
2013-12-22 15:31:53 +0400

    Border-radius iOs fix.

--- run.sh
+++ run.sh
@@ -1,5 +1,20 @@
+ENV=${1:-DEV}
+echo Starting with ENV: $ENV
+
 kill -s 9 $(./pidof grunt)
+
 cd client
-grunt watch &
+
+if [ $ENV = "DEV" ]; then
+  echo "Starting grunt watch"
+  grunt watch &
+fi
+
+if [ $ENV = "production" ]; then
+  echo "Building project"
+  grunt build
+fi
+
+
 cd ../
-NODE_ENV=DEV nodemon simple-server/server.js
+NODE_ENV=$ENV nodemon simple-server/server.js
